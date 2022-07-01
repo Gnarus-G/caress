199a3c503522270679c1c7efa10aa5cac709219e
gnaru-g <g.avet.gnaritas@gmail.com>
2022-07-01 19:53:37 -0400

    chore: add LICENSE to final tar build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cargo build --all --release;
 strip target/release/$crate;
 mv target/release/$crate $crate
 
-tar -czvf $crate-$OSTYPE.tar.gz $crate README.md
+tar -czvf $crate-$OSTYPE.tar.gz $crate README.md LICENSE
